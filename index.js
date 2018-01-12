0608eabd5b459c9aefd82bd0d1bd11cbcd292761
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-12 03:38:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, name) {
+  return `It\'s ${number} days until ${holiday}!`
+}
