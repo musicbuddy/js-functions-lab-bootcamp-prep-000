31f3c372d2a5f3b764e09b08d24c6dcca9d1edc4
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-12 05:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,3 @@
 function happyHolidays() {
   return "Happy holidays!";
 }
-
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${Ociel}!`;
-}
-
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${Mothers day}, ${Teresa}!`
-}
-
-function holidayCountdown(holiday, days) {
-  return `It's ${20} days until ${Christmas}!`
-}
